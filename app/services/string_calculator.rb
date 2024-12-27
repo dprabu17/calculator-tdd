e28722c25e271b1c84d54f9f03a91d12590c072c
Prabu <prabud@spritle.com>
2024-12-27 11:24:17 +0530

    unit test sum of the numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,5 +1,23 @@
 class StringCalculator
-  def self.add(input)
-    return 0 if input.empty?
+  def self.add(params_str)
+    return 0 if params_str.empty?
+
+    delimiters, params_str = custom_delimiter(params_str)
+
+    given_inputs = params_str.split(delimiters).map(&:to_i)
+    negatives = given_inputs.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+    result = given_inputs.sum
+    return result
+  end
+
+  def self.custom_delimiter(input_string)
+    delimiters = /,|\n/
+    if input_string.start_with?("//")
+      delimiter, input_string = input_string[2..].split("\n", 2)
+      delimiters = /#{delimiter}/
+    end
+    return [delimiters, input_string]
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,20 @@ RSpec.describe StringCalculator, type: :model do
     it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "when given a single number return that same number" do
+      expect(StringCalculator.add("3")).to eq(3)
+      expect(StringCalculator.add("6")).to eq(6)
+      expect(StringCalculator.add("9")).to eq(9)
+    end
+
+    it "when given a two comma-separated numbers return the sum of two numbers" do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+    it "when given the multiple comma-separated numbers return the sum of the numbers" do
+      expect(StringCalculator.add("1,2,3")).to eq(6)
+    end
+    
   end
 end
