edffdadcb75132fcac92f12f79fc7bc3d944f564
Prabu <prabud@spritle.com>
2024-12-27 12:04:38 +0530

    unit test when input contains custom delimiter

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -19,6 +19,23 @@ RSpec.describe StringCalculator, type: :model do
     it "when given the multiple comma-separated numbers return the sum of the numbers" do
       expect(StringCalculator.add("1,2,3")).to eq(6)
     end
-    
+
+    it "raises an exception for negative numbers" do
+      expect { StringCalculator.add("1,-5,3") }.to raise_error("negative numbers not allowed -5")
+    end
+
+    it "raises an exception for multiple negative numbers" do
+      expect { StringCalculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed -2, -3")
+    end
+
+    it "when given the numbers separated by newlines returns the sum of numbers" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+
+    it "when given the numbers separated by custom delimiters returns the sum of numbers" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
   end
 end
